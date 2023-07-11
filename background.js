9251fe297acb11b923af6133a4f4c5229bfcb5bc
pawan-git7979 <63632573+pawan-git7979@users.noreply.github.com>
2023-07-11 22:59:32 +0530

    Add files via upload

---
+++ background.js
@@ -0,0 +1,7 @@
+// background.js
+// Author: Pawan Agrawal
+// Author URI: https://
+// Author Github URI: https://www.github.com/pawan-git7979
+// Project Repository URI: https://github.com/pawan-git7979/Problems_ToDo
+// Description: Handles all the browser level activities (e.g. tab management, etc.)
+// License: MIT
